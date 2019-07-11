01ae125aa0b704e90c7d63678e0855fe4864235e
rodion <rodion.prosvirnin@bk.ru>
2019-07-12 01:37:40 +0300

    just created simple activity TODO: create broadcast receiver

---
+++ app/src/main/java/ru/skillbranch/devintensive/workmanagerdemonstrativerepo/broadcastreceiver/BroadcastActivity.java
@@ -0,0 +1,29 @@
+package ru.skillbranch.devintensive.workmanagerdemonstrativerepo.broadcastreceiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import ru.skillbranch.devintensive.workmanagerdemonstrativerepo.R;
+
+public class BroadcastActivity extends AppCompatActivity {
+
+    private RecyclerView recycler;
+    private TextView text;
+    private RecyclerView.LayoutManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast);
+
+        text = findViewById(R.id.nothing_broadcast);
+
+        recycler = findViewById(R.id.recycler_broadcast);
+        manager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(manager);
+        recycler.setHasFixedSize(true);
+    }
+}
