59d000c621a725bcd8732b40d6d47db41a363070
rodion <rodion.prosvirnin@bk.ru>
2019-07-13 01:54:42 +0300

    service binding

--- app/src/main/java/ru/skillbranch/devintensive/workmanagerdemonstrativerepo/service/codetutor/MyService.java
+++ app/src/main/java/ru/skillbranch/devintensive/workmanagerdemonstrativerepo/service/codetutor/MyService.java
@@ -2,26 +2,78 @@ package ru.skillbranch.devintensive.workmanagerdemonstrativerepo.service.codetut
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import java.util.Random;
+
 public class MyService extends Service {
+
+    private int mRandomNumber;
+    private boolean mIsRandomGeneratorOn;
+
+    private final int MIN=0;
+    private final int MAX=100;
+
+    public static final int GET_COUNT=0;
+
+    class MyServiceBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    private IBinder binder = new MyServiceBinder();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        mIsRandomGeneratorOn =true;
+        new Thread(this::startRandomNumberGenerator).start();
+        return START_STICKY;
+    }
 
-        stopSelf();
-
-        return super.onStartCommand(intent, flags, startId);
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        //stop generator
+        stopRandomNumberGenerator();
+    }
+
+    private void startRandomNumberGenerator(){
+        while (mIsRandomGeneratorOn){
+            try{
+                Thread.sleep(1000);
+                if(mIsRandomGeneratorOn){
+                    mRandomNumber = new Random().nextInt(MAX)+MIN;
+                }
+            }catch (InterruptedException e){
+            }
+
+        }
+    }
+
+    private void stopRandomNumberGenerator(){
+        mIsRandomGeneratorOn =false;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    public int getRandomNumber(){
+        return mRandomNumber;
     }
 }
